b80c4b3344d171b5cea87d539b0e5022cfa68663
ABDELHAFID KHALIL <130927691+abdelhafid200@users.noreply.github.com>
2024-10-27 17:58:43 +0100

    Update environment.ts
    update environnement

--- client/src/app/environments/environment.ts
+++ client/src/app/environments/environment.ts
@@ -1,11 +1,6 @@
 // environment.ts
 export const environment = {
-    production: false,
-    GEMINI_API_KEY: 'AIzaSyDKifKpsX--',
-    googleClientId: '424280322033-ugsil9ftohumpd7c0bl13v8a69ilj4s4.apps.googleusercontent.com',
-    googleClientSecret: 'GOCSPX-',
-    googleApiKey: 'AIzaSyDKifKpsX-L2RnvRb8usnq7A-',
-    x_key: 'ab979fa1-a199-4129',
+    
   };
 
 
